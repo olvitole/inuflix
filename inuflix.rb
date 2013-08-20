ab11eb354dfefa65718b41f5f8151322382f384d
tazro inutano ohta <inutano@gmail.com>
2013-08-20 18:18:09 +0900

    no more cypher

--- inuflix.rb
+++ inuflix.rb
@@ -2,24 +2,17 @@
 
 require "neography"
 require "sinatra"
-require "haml"
 require "yaml"
+require "json"
 
 config = YAML.load_file("./config.yaml")
 neo = Neography::Rest.new(ENV['NEO4J_URL'] || config["neo4j_server"])
 
-def search_concept(neo, namespace, keyword)
-  w3type = "http://www.w3.org/1999/02/22-rdf-syntax-ns\#type"
-  cypher_query =  " START x=node:`#{namespace}`(\"name:#{keyword}\")"
-  cypher_query << " MATCH (x)-[?:`#{w3type}`]-(t)"
-  cypher_query << " RETURN x.uri, t.name"
-  cypher_query << " LIMIT 100"
-  neo.execute_query(cypher_query)["data"]
-end
-
-get "/search_concept" do
-  content_type :json
-  search_concept(neo, params[:namespace], params[:keyword]).to_json
+post "/exec" do
+  request.body.rewind
+  data = JSON.parse(request.body.read)
+  query = data["query"]
+  neo.execute_query(query)["data"].to_json
 end
 
 get "/" do
