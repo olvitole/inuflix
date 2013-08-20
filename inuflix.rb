6c544163374f17603eb69bdf8e60646dbb01ab6f
tazro inutano ohta <inutano@gmail.com>
2013-08-20 16:53:51 +0900

    minimum test app

--- inuflix.rb
+++ inuflix.rb
@@ -2,30 +2,21 @@
 
 require "neography"
 require "sinatra"
-require "haml"
 
 neo = Neography::Rest.new(ENV['NEO4J_URL'] || "http://localhost:7474/")
 
-def query_builder(namespace, keyword)
+def search_concept(neo, namespace, keyword)
   w3type = "http://www.w3.org/1999/02/22-rdf-syntax-ns\#type"
-  cypher_query =  " START x=node:`#{namespace}`(name = '#{keyword}')"
+  cypher_query =  " START x=node:`#{namespace}`(\"name:#{keyword}\")"
   cypher_query << " MATCH (x)-[?:`#{w3type}`]-(t)"
   cypher_query << " RETURN x.uri, t.name"
   cypher_query << " LIMIT 100"
+  neo.execute_query(cypher_query)["data"]
 end
 
-def execute_cypher(neo, namespace, keyword)
-  cypher_query = query_builder(namespace, keyword)
-  neo.execute_query(cypher_query)["data"].to_json
-end
-
-get "/exec_cypher" do
+get "/search_concept" do
   content_type :json
-  execute_cypher(neo, "http://tcng.hgc.jp/", "FOXA1")
-end
-
-get "/build_query" do
-  query_builder("http://tcng.hgc.jp/", "FOXA1")
+  search_concept(neo, params[:namespace], params[:keyword]).to_json
 end
 
 get "/" do
