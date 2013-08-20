c98fd12af9577a80cc3be13407e1869c9271f7e8
tazro inutano ohta <inutano@gmail.com>
2013-08-20 12:11:33 +0900

    enabled neography

--- inuflix.rb
+++ inuflix.rb
@@ -1,10 +1,10 @@
 # coding for Ruby 2.0
 
-#require "neography"
+require "neography"
 require "sinatra"
 require "haml"
 
-#neo = Neography::Rest.new(ENV["NEO4J_URL"] || "http://localhost:7474")
+neo = Neography::Rest.new
 
 def execute_cypher(namespace, keyword)
   w3type = "http://www.w3.org/1999/02/22-rdf-syntax-ns\#type"
@@ -13,15 +13,12 @@ def execute_cypher(namespace, keyword)
   cypher_query << " RETURN x.uri, t.name"
   cypher_query << " LIMIT 100"
   puts cypher_query
-  #neo.execute_query(query)["data"]
+  neo.execute_query(query)["data"]
 end
-
-execute_cypher("http://tcng.hgc.jp/","FOXA1")
-exit
 
 get "/exec_cypher" do
   content_type :json
-  execute_cypher("http://ccle.acme.org/","*RAS")
+  execute_cypher("http://ccle.acme.org/","FOXA1")
 end
 
 get "/" do
