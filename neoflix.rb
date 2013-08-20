a574a0d5aba92174a12a976d79533d0efbb7668e
tazro inutano ohta <inutano@gmail.com>
2013-08-20 11:57:29 +0900

    cleaned messy lines

--- neoflix.rb
+++ neoflix.rb
@@ -11,12 +11,11 @@ Tmdb.default_language = "en"
 neo = Neography::Rest.new(ENV['NEO4J_URL'] || "http://localhost:7474")
 
 def create_graph(neo)
-    begin
-     neo.execute_script("g.idx('vertices')[[type:'Movie']].count();").to_i > 0
-     return 
-    rescue
-      puts "Creating Database"
-    end
+  begin
+    return if neo.execute_script("g.idx('vertices')[[type:'Movie']].count();").to_i > 0
+  rescue
+    puts "Creating Database"
+  end
   
   if neo.execute_script("g.indices;").empty?  
     neo.execute_script("g.createAutomaticIndex('vertices', Vertex.class, null);") 
@@ -25,37 +24,36 @@ def create_graph(neo)
 
   begin
     neo.execute_script("g.setMaxBufferSize(1000);
+                       'http://neoflix.heroku.com/movies.dat'.toURL().eachLine { def line ->
+                         def components = line.split('::');
+                         def movieVertex = g.addVertex(['type':'Movie', 'movieId':components[0].toInteger(), 'title':components[1]]);
+                         components[2].split('\\\\|').each { def genera ->
+                           def hits = g.idx(Tokens.T.v)[[genera:genera]].iterator();
+                           def generaVertex = hits.hasNext() ? hits.next() : g.addVertex(['type':'Genera', 'genera':genera]);
+                           g.addEdge(movieVertex, generaVertex, 'hasGenera');
+                         }
+                       };
 
-                    'http://neoflix.heroku.com/movies.dat'.toURL().eachLine { def line ->
-                       def components = line.split('::');
-                       def movieVertex = g.addVertex(['type':'Movie', 'movieId':components[0].toInteger(), 'title':components[1]]);
-                       components[2].split('\\\\|').each { def genera ->
-                         def hits = g.idx(Tokens.T.v)[[genera:genera]].iterator();
-                         def generaVertex = hits.hasNext() ? hits.next() : g.addVertex(['type':'Genera', 'genera':genera]);
-                         g.addEdge(movieVertex, generaVertex, 'hasGenera');
-                       }
-                     };
+                       occupations = [0:'other', 1:'academic/educator', 2:'artist',
+                         3:'clerical/admin', 4:'college/grad student', 5:'customer service',
+                         6:'doctor/health care', 7:'executive/managerial', 8:'farmer',
+                         9:'homemaker', 10:'K-12 student', 11:'lawyer', 12:'programmer',
+                         13:'retired', 14:'sales/marketing', 15:'scientist', 16:'self-employed',
+                         17:'technician/engineer', 18:'tradesman/craftsman', 19:'unemployed', 20:'writer'];
 
-                     occupations = [0:'other', 1:'academic/educator', 2:'artist',
-                       3:'clerical/admin', 4:'college/grad student', 5:'customer service',
-                       6:'doctor/health care', 7:'executive/managerial', 8:'farmer',
-                       9:'homemaker', 10:'K-12 student', 11:'lawyer', 12:'programmer',
-                       13:'retired', 14:'sales/marketing', 15:'scientist', 16:'self-employed',
-                       17:'technician/engineer', 18:'tradesman/craftsman', 19:'unemployed', 20:'writer'];
+                       'http://neoflix.heroku.com/users.dat'.toURL().eachLine { def line ->
+                         def components = line.split('::');
+                         def userVertex = g.addVertex(['type':'User', 'userId':components[0].toInteger(), 'gender':components[1], 'age':components[2].toInteger()]);
+                         def occupation = occupations[components[3].toInteger()];
+                         def hits = g.idx(Tokens.T.v)[[occupation:occupation]].iterator();
+                         def occupationVertex = hits.hasNext() ? hits.next() : g.addVertex(['type':'Occupation', 'occupation':occupation]);
+                         g.addEdge(userVertex, occupationVertex, 'hasOccupation');
+                       };
 
-                     'http://neoflix.heroku.com/users.dat'.toURL().eachLine { def line ->
-                       def components = line.split('::');
-                       def userVertex = g.addVertex(['type':'User', 'userId':components[0].toInteger(), 'gender':components[1], 'age':components[2].toInteger()]);
-                       def occupation = occupations[components[3].toInteger()];
-                       def hits = g.idx(Tokens.T.v)[[occupation:occupation]].iterator();
-                       def occupationVertex = hits.hasNext() ? hits.next() : g.addVertex(['type':'Occupation', 'occupation':occupation]);
-                       g.addEdge(userVertex, occupationVertex, 'hasOccupation');
-                     };
-
-                     'http://neoflix.heroku.com/ratings.dat'.toURL().eachLine {def line ->
-                       def components = line.split('::');
-                       def ratedEdge = g.addEdge(g.idx(Tokens.T.v)[[userId:components[0].toInteger()]].next(), g.idx(T.v)[[movieId:components[1].toInteger()]].next(), 'rated');
-                       ratedEdge.setProperty('stars', components[2].toInteger());
+                       'http://neoflix.heroku.com/ratings.dat'.toURL().eachLine {def line ->
+                         def components = line.split('::');
+                         def ratedEdge = g.addEdge(g.idx(Tokens.T.v)[[userId:components[0].toInteger()]].next(), g.idx(T.v)[[movieId:components[1].toInteger()]].next(), 'rated');
+                         ratedEdge.setProperty('stars', components[2].toInteger());
                        };
 
                       g.stopTransaction(TransactionalGraph.Conclusion.SUCCESS);")
