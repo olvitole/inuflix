e086031907ec219e435e1fde4d37007034dcdb99
tazro inutano ohta <inutano@gmail.com>
2013-08-20 12:55:07 +0900

    fixed cypher query

--- inuflix.rb
+++ inuflix.rb
@@ -4,20 +4,28 @@ require "neography"
 require "sinatra"
 require "haml"
 
-neo = Neography::Rest.new
+neo = Neography::Rest.new(ENV['NEO4J_URL'] || "http://localhost:7474/")
 
-def execute_cypher(neo, namespace, keyword)
+def query_builder(namespace, keyword)
   w3type = "http://www.w3.org/1999/02/22-rdf-syntax-ns\#type"
   cypher_query =  " START x=node:`#{namespace}`(name = '#{keyword}')"
-  cypher_query << " MATCH (x)-[?:#{w3type}]-(t)"
+  cypher_query << " MATCH (x)-[?:`#{w3type}`]-(t)"
   cypher_query << " RETURN x.uri, t.name"
   cypher_query << " LIMIT 100"
+end
+
+def execute_cypher(neo, namespace, keyword)
+  cypher_query = query_builder(namespace, keyword)
   neo.execute_query(cypher_query)["data"]
 end
 
 get "/exec_cypher" do
   content_type :json
-  execute_cypher(neo, "http://ccle.acme.org/","FOXA1")
+  execute_cypher(neo, "http://tcng.hgc.jp/", "FOXA1")
+end
+
+get "/build_query" do
+  query_builder("http://tcng.hgc.jp/", "FOXA1")
 end
 
 get "/" do
