a037c494b3746fc173556733dcc9a892bacef472
tazro inutano ohta <inutano@gmail.com>
2014-01-17 16:28:28 +0900

    crosssssssssssssssssss

--- app.rb
+++ app.rb
@@ -2,8 +2,11 @@
 
 require "neography"
 require "sinatra"
+require "sinatra/cross_origin"
 require "yaml"
 require "uri"
+
+enable :cross_origin
 
 neo = Neography::Rest.new(ENV['NEO4J_URL'] || YAML.load_file("./config.yaml")["neo4j_server"])
 
@@ -11,6 +14,7 @@ post "/exec" do
   request.body.rewind
   query = URI.decode(request.body.read.sub(/^query=/,"")).gsub("+","\s")
   content_type :json
+  cross_origin
   neo.execute_query(query).to_json
 end
 
